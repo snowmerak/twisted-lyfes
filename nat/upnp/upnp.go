560d371a06e1a56115ce98a3f87126ca295a90c8
snowmerak <snowmerak@outlook.com>
2021-12-16 14:25:46 +0900

    Create upnp.go

---
+++ nat/upnp/upnp.go
@@ -0,0 +1,56 @@
+package upnp
+
+import (
+	"context"
+	"errors"
+	"log"
+
+	"github.com/twisted-lyfes/utility/net/port"
+	"gitlab.com/NebulousLabs/go-upnp"
+)
+
+type UPnP struct {
+	d *upnp.IGD
+	p uint16
+}
+
+func New(ctx context.Context) (*UPnP, error) {
+	u := new(UPnP)
+
+	p := port.MIN
+
+	d, err := upnp.DiscoverCtx(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	for {
+		if err := d.Forward(p, "peer2peer"); err != nil {
+			p++
+			if p > port.MAX {
+				return nil, errors.New("no avaliable port")
+			}
+			continue
+		}
+		if port.IsAvaliable(p) {
+			break
+		}
+	}
+
+	u.d = d
+	u.p = p
+
+	return u, nil
+}
+
+func (u *UPnP) Clear() error {
+	if err := u.d.Clear(u.p); err != nil {
+		return err
+	}
+	log.Printf("Cleared port %d\n", u.p)
+	return nil
+}
+
+func (u *UPnP) Port() uint16 {
+	return u.p
+}
