c9e5fb11b4a8a1751626609251f35e4a407a5d66
snowmerak <snowmerak@outlook.com>
2023-04-02 13:44:02 +0900

    reduce name

--- db/embedded/embedded.go
+++ db/embedded/embedded.go
@@ -2,7 +2,7 @@ package embedded
 
 import "google.golang.org/protobuf/reflect/protoreflect"
 
-type EmbeddedDB interface {
+type Embedded interface {
 	Put(subject string, key string, data protoreflect.ProtoMessage) error
 	Get(subject string, key string, value protoreflect.ProtoMessage) (protoreflect.ProtoMessage, error)
 	PutWithExpire(subject string, ttl int64, timestamp int64, key string, data protoreflect.ProtoMessage) error
