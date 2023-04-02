b7f5577e32d98b4eeec6d4d7e41da0b174d5a300
snowmerak <snowmerak@outlook.com>
2023-04-02 16:56:58 +0900

    fix proto dto dirs

--- src/db/fsys/fsys.go
+++ src/db/fsys/fsys.go
@@ -1,9 +1,25 @@
 package fsys
 
+import (
+	"google.golang.org/protobuf/proto"
+	"os"
+	"path/filepath"
+)
+
 type FSys struct {
 	basePath string
 }
 
-func (f *FSys) GetMetaData(name string) (map[string]bool, error) {
+func (fs *FSys) GetMetaData(name string) (*MetaData, error) {
+	data, err := os.ReadFile(filepath.Join(fs.basePath, name, MetaDataFileName))
+	if err != nil {
+		return nil, err
+	}
 
+	md := new(MetaData)
+	if err := proto.Unmarshal(data, md); err != nil {
+		return nil, err
+	}
+
+	return md, nil
 }
